234a74d39a401283a1c8d997bfa1ee91c68f1255
Nick <ngideo@gmail.com>
2017-11-30 15:36:05 -0600

    parsing and tokens working

--- src/slip.test.ts
+++ src/slip.test.ts
@@ -1,8 +1,38 @@
-import {tokenize} from './slip';
+import {tokenize, parse, atom} from './slip';
 
 it("tokenizes", () => {
   expect(tokenize("(+ 1 2)"))
     .toEqual([ '(', '+', '1', '2', ')']);
   expect(tokenize('(+ (- 1 2) 3)'))
     .toEqual(['(', '+', '(', '-', '1', '2', ')', '3', ')', ]);
+})
+
+/*
+it('replaces', () => {
+  expect( "()".replace('(', ' ( ') )
+    .toEqual(" ( )");
+
+  expect( "()".replace(')', ' ) ') )
+    .toEqual("( ) ");
+
+  expect( "()".replace('(', ' ( ')
+              .replace(')', ' ) ')  )
+    .toEqual(" (  ) ");
+  
+  expect( "())".replace(/\(/g, ' ( ')
+              .replace( /\)/g , ' ) ')  )
+    .toEqual(" (  )  ) ");
+})
+*/
+
+it('atomizes', () => {
+  expect(atom('3'))
+  .toEqual(3)
+  expect(atom('*'))
+    .toEqual('*')
+});
+
+it('parses', () => {
+  expect(parse("(begin (define r 10) (* pi (* r r)))"))
+    .toEqual(['begin', ['define', 'r', 10], ['*', 'pi', ['*', 'r', 'r']]]);
 })
--- src/slip.ts
+++ src/slip.ts
@@ -4,17 +4,45 @@
  */
 
 
+interface message {
+  token: string
+  type: string
+}
+
 /**
- * Atom =  Number | Symbol
- */
+ * Atom =  Number | Symbol | String
+ * 
+ * NOTE: this is set up to return strings, but
+ * string parsing needs to be implemented in 
+ * readFromTokens()
+ 
 class Atom {
-  constructor(token: string){
-    let num = new Number(token);
-    if (num !== NaN)
-      return num;
-    else
-      return Symbol(token);
+  constructor(msg: message){
+    if (msg.type === 'string'){
+      return msg.token;
+    }
+    else if (msg.type === 'other'){
+      let num = new Number(msg.token);
+      if (!isNaN(num))
+        return num;
+      else
+        return Symbol(msg.token);
+    }
   }
+}
+
+*/
+
+
+/**
+ * Norvigs implementation
+ */
+export function atom(token: string): number | string {
+  var num = Number(token);
+  if (!isNaN(num))
+    return num;
+  else
+    return token;
 }
 
 type List = Array<Atom>;
@@ -23,39 +51,88 @@ type Env = Object;
 
 
 export function tokenize(chars: string): string[] {
-  return chars.split('')
-              .filter( v => v !== ' ');
+  return chars.replace(/\(/g, ' ( ')
+              .replace(/\)/g, ' ) ')
+              .split(' ')
+              .filter( v => v !== '');
+}
+
+export function tokenize2(chars: string): string[] {
+  var tokens = [],
+    line = 0,
+    column = 0,
+    wordEnd = 0,
+    c = '';
+
+  for(var i = 0; i < chars.length; i++){
+    c = chars.charAt(i);
+    if(c === ' '){ //case for \t ???
+      column = i;
+      continue;
+    } else if (c === '\n' || c === '\b') {
+      column = 0;
+      line++;
+      continue;
+    } else if (c === '(') {
+      tokens.push(c);
+      column = i;
+      continue;
+    }
+
+  }
+
 }
 
 
 export function parse(program: string) {
- return readFromTokens(tokenize(program));
+    return readFromTokens(tokenize(program));
 }
 
+
+/**
+ * TODO: need to implement string parsing..
+ * 
+ * @param tokens 
+ */
 export function readFromTokens(tokens: string[]): Exp {
- if (tokens.length === 0) {
-   throw new SyntaxError('Unexpected EOF');
- }
- var token = tokens.shift();
- 
- if (token === '('){
-   var L = new Array<Atom>();
-   while (tokens[0] !== ')') 
-     L.push(readFromTokens(tokens));
-   tokens.shift();
-   return L;
- }
- else if (token === ')')
-   throw new SyntaxError('Unexpected )');
- else
-   return new Atom(token) // direct copy of norvigs code.
-   //is atom() a constructor? 
- 
+  if (tokens.length === 0) {
+    throw new SyntaxError('Unexpected EOF');
+  }
+
+  var token = tokens.shift();
+
+  if (token === undefined) 
+    return ''; //This is to appease the type-checker.. better way?
+  else if (token === '(') {
+
+    //var L = new Array<Atom>();
+
+    var L = [];
+    while (tokens[0] !== ')') 
+      L.push(readFromTokens(tokens));
+    tokens.shift(); //kicks out ')'
+    return L;
+  }
+  else if (token === ')')
+    throw new SyntaxError('Unexpected )');
+  else
+    return atom(token) 
 }
 
 
-function slip (programInput) {
+function slip () {
 
+  //get program from textbox or cmd line
+  //TODO: write getInput();
+  var input = getInput();
+
+  try { 
+    var program = parse(input) 
+  }
+  catch(e){
+    console.warn(e.message);
+    return '';
+  }
 }
 
 
